3058bf86b8a3e450eeb07578a2796bf933eea446
sysnxw <44738356+sysnxw@users.noreply.github.com>
2020-07-21 14:03:01 +0800

    Update pthome.cookie.js

--- pthome/pthome.cookie.js
+++ pthome/pthome.cookie.js
@@ -1,67 +1,15 @@
 const cookieName = 'pthome'
-const signurlKey = 'senku_signurl_pthome'
-const signheaderKey = 'senku_signheader_pthome'
-const signbodyKey = 'senku_signbody_pthome'
-const senku = init()
+const cookieKey = 'chavy_cookie_pthome'
+const cookieVal = $request.headers['Cookie']
 
-const requrl = $request.url
-if ($request && $request.method != 'OPTIONS') {
-  const signurlVal = requrl
-  const signheaderVal = JSON.stringify($request.headers)
-  const signbodyVal = $request.body
-  const cmd = JSON.parse($request.body).cmd
-  senku.log(`signurlVal:${signurlVal}`)
-  senku.log(`signheaderVal:${signheaderVal}`)
-  senku.log(`signbodyVal:${signbodyVal}`)
-  if (signurlVal) senku.setdata(signurlVal, signurlKey)
-  if (signheaderVal) senku.setdata(signheaderVal, signheaderKey)
-  if (signbodyVal && cmd=='task.revisionSignInGetAward') {
-    senku.setdata(signbodyVal, signbodyKey)
-    senku.msg(cookieName, `获取Cookie: 成功`, ``)
-  }  
+if (cookieVal) {
+  let cookie = $persistentStore.write(cookieVal, cookieKey)
+  if (cookie) {
+    let msg = `${cookieName}`
+    $notification.post(msg, 'Cookie写入成功', '详见日志')
+    console.log(msg)
+    console.log(cookieVal)
+  }
 }
 
-function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-    }
-  }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-    }
-  }
-  done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
-}
-senku.done()
+$done({})
