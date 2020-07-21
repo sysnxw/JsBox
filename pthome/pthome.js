9d09494266ba82d1675313a0bfb0c4ac5ed370e4
sysnxw <44738356+sysnxw@users.noreply.github.com>
2020-07-21 14:19:22 +0800

    Update pthome.js

--- pthome/pthome.js
+++ pthome/pthome.js
@@ -1,77 +1,49 @@
 const cookieName = 'pthome'
-const signurlKey = 'senku_signurl_pthome'
-const signheaderKey = 'senku_signheader_pthome'
-const signbodyKey = 'senku_signbody_pthome'
-const senku = init()
-const signurlVal = senku.getdata(signurlKey)
-const signheaderVal = senku.getdata(signheaderKey)
-const signBodyVal = senku.getdata(signbodyKey)
-
-sign()
+const cookieKey = 'chavy_cookie_pthome'
+const cookieVal = $persistentStore.read(cookieKey)
 
 function sign() {
-  const url = { url: signurlVal, headers: JSON.parse(signheaderVal), body: signBodyVal }
-  senku.get(url, (error, response, data) => {
-    const result = JSON.parse(data)
-    //console.log(result) 
-    //const total = result.data['task.revisionSignInGetAward'].total
-    //const ret = result.data['task.revisionSignInGetAward'].ret
-    let subTitle = `执行了但是不知道是否正常`
-    let detail = `不知道哦`
-    /*if (total != 0) {
-      const num = result.data['task.revisionSignInGetAward'].awards[0].num
-      subTitle = `签到结果: 成功`
-      detail = `获得鲜花: ${num}朵,已连续签到:${total}天`
-    } else if (ret == -11532) {
-      subTitle = `签到结果: 成功 (重复签到)`
+  let url = {
+    url: `https://www.pthome.net/attendance.php`,
+    headers: {
+      Cookie: cookieVal
+    }
+  }
+  $httpClient.get(url, (error, response, data) => {
+    if (data.indexOf('今天已经签到过了') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到跳过`
+      let detail = `今天已经签过了`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notification.post(title, subTitle, detail)
     } else {
-      subTitle = `签到结果: 失败`
-    }*/
-    senku.msg(cookieName, subTitle, detail)
-    senku.done()
+      //signMission(data.match(/<input[^>]*\/mission\/daily\/redeem\?once=(\d+)[^>]*>/)[1])
+      console.log(`${title}, ${subTitle}, ${detail}`)
+    }
+  })
+  $done({})
+}
+
+function signMission(code) {
+  let url = {
+    url: `https://www.v2ex.com/mission/daily/redeem?once=${code}`,
+    headers: { Cookie: cookieVal }
+  }
+  $httpClient.get(url, (error, response, data) => {
+    if (data.indexOf('今天已经签到过了') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到成功`
+      let detail = ``
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notification.post(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到失败`
+      let detail = `详见日志`
+      console.log(`签到失败: ${cookieName}, error: ${error}, response: ${response}, data: ${data}`)
+      $notification.post(title, subTitle, detail)
+    }
   })
 }
 
-function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
-    }
-  }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
-    }
-  }
-  done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
-}
+sign({})
