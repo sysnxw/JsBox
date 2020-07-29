46535dc6cd707e0114b7f87e00fdabb2cfddcb58
sysnxw <44738356+sysnxw@users.noreply.github.com>
2020-07-29 16:43:13 +0800

    Update pthome.js

--- pthome/pthome.js
+++ pthome/pthome.js
@@ -1,46 +1,49 @@
 const cookieName = 'pthome'
 const cookieKey = 'chavy_cookie_pthome'
-const cookieVal = $persistentStore.read(cookieKey)
+const cookieVal = $prefs.valueForKey(cookieKey)
 
 function sign() {
   let url = {
     url: `https://www.pthome.net/index.php`,
+    method: 'GET',
     headers: {
       Cookie: cookieVal
     }
   }
-  $httpClient.get(url, (error, response, data) => {
+  $task.fetch(url).then((response) => {
+    let data = response.body
     if (data.indexOf('签到得魔力') <= 0) {
       let title = `${cookieName}`
       let subTitle = `签到结果: 签到跳过`
       let detail = `今天已经签过了`
       console.log(`${title}, ${subTitle}, ${detail}`)
-      $notification.post(title, subTitle, detail)
+      $notify(title, subTitle, detail)
     } else {
-      signMission()//找到点击签到的点击，配合下面的signMission（code）来真正签到
+      signMission()
     }
   })
-  $done({})
 }
 
-function signMission() {
+function signMission(code) {
   let url = {
     url: `https://www.pthome.net/attendance.php`,
+    method: 'GET',
     headers: { Cookie: cookieVal }
   }
-  $httpClient.get(url, (error, response, data) => {
+  $task.fetch(url).then((response) => {
+    let data = response.body
     if (data.indexOf('签到得魔力') <= 0) {
       let title = `${cookieName}`
       let subTitle = `签到结果: 签到成功`
       let detail = ``
       console.log(`${title}, ${subTitle}, ${detail}`)
-      $notification.post(title, subTitle, detail)
+      $notify(title, subTitle, detail)
     } else {
       let title = `${cookieName}`
       let subTitle = `签到结果: 签到失败`
       let detail = `详见日志`
-      console.log(`签到失败: ${cookieName}, error: ${error}, response: ${response}, data: ${data}`)
-      $notification.post(title, subTitle, detail)
+      console.log(`签到失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
     }
   })
 }
