927c26ca0dce2a9d75216ab2ab9413fdcfcc299f
sysnxw <44738356+sysnxw@users.noreply.github.com>
2020-07-29 16:51:23 +0800

    Create pthome.js

---
+++ pthome/pthome.js
@@ -0,0 +1,48 @@
+const cookieName = 'pthome'
+const cookieKey = 'chavy_cookie_pthome'
+const cookieVal = $persistentStore.read(cookieKey)
+
+function sign() {
+  let url = {
+    url: `https://www.pthome.net/index.php`,
+    headers: {
+      Cookie: cookieVal
+    }
+  }
+  $httpClient.get(url, (error, response, data) => {
+    if (data.indexOf('签到得魔力') <= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到跳过`
+      let detail = `今天已经签过了`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notification.post(title, subTitle, detail)
+    } else {
+      signMission()//找到点击签到的点击，配合下面的signMission（code）来真正签到
+    }
+  })
+  $done({})
+}
+
+function signMission() {
+  let url = {
+    url: `https://www.pthome.net/attendance.php`,
+    headers: { Cookie: cookieVal }
+  }
+  $httpClient.get(url, (error, response, data) => {
+    if (data.indexOf('签到得魔力') <= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到成功`
+      let detail = ``
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notification.post(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到失败`
+      let detail = `详见日志`
+      console.log(`签到失败: ${cookieName}, error: ${error}, response: ${response}, data: ${data}`)
+      $notification.post(title, subTitle, detail)
+    }
+  })
+}
+
+sign({})
